ed034c8f4aeabff43c6e044e68558c9ff7fd8730
Matthew Todor <unclejunglegeorge@gmail.com>
2023-01-06 15:08:03 -0700

    removed a comment in connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// aceo database has placeholder "user" collection for now.
 mongoose.connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/aceo`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
